f20aaf56b8483a8f221e203b12c531ef63e3d7c8
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-11 19:14:09 -0400

    Make Colors More Aesthetic and Add Hex Label

--- src/generator/Color.tsx
+++ src/generator/Color.tsx
@@ -4,7 +4,12 @@ interface Props {
 
 const Color = ({ color }: Props) => {
   return (
-    <div style={{ backgroundColor: `${color}` }} className={`w-full h-full`}></div>
+    <div
+      style={{ backgroundColor: `#${color}` }}
+      className={`flex justify-center items-end pb-28 w-full h-full`}
+    >
+      <h1 className="text-3xl">{color}</h1>
+    </div>
   );
 };
 
--- src/utils/generateColor.ts
+++ src/utils/generateColor.ts
@@ -1,13 +1,38 @@
 export const generateColor = () => {
-  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = hslToHex(
+    randomInt(0, 360),
+    randomInt(42, 98),
+    randomInt(40, 90)
+  ).toUpperCase();
   return color;
 };
 
 export const generateColors = (amount = 1) => {
   const colors: string[] = [];
   Array.from({ length: amount }).forEach(() => {
-    const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const color = hslToHex(
+      randomInt(0, 360),
+      randomInt(42, 98),
+      randomInt(40, 90)
+    ).toUpperCase();
     colors.push(color);
   });
   return colors;
 };
+
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function hslToHex(h: number, s: number, l: number) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `${f(0)}${f(8)}${f(4)}`;
+}
