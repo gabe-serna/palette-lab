2f723152f56726eb31bf3fb644ee888f39d88078
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-11 16:24:36 -0400

    Add Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid md:grid-rows-[3.75rem_3.5rem_calc(100vh-3.75rem-3.5rem)] grid-rows-[3.75rem_calc(100vh-3.75rem-3.5rem)_3.5rem]">
+        <div className="flex items-center row-span-1 row-start-1 border border-white">
+          <h1>App Name</h1>
+        </div>
+        <div className="flex items-center row-span-1 row-start-3 border border-white md:row-start-2">
+          Sub Header
+        </div>
+        <div className="row-span-1 row-start-2 border border-white md:row-start-3">
+          <h2>Body</h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
