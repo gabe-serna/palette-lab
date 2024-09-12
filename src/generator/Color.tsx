5052806719b12f173cf99436f2e5f43dcf2a1764
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-11 20:06:04 -0400

    Add Hex Label Names

--- src/generator/Color.tsx
+++ src/generator/Color.tsx
@@ -1,4 +1,6 @@
 import { getTextColor } from "../utils/getTextColor";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface Props {
   color: string;
@@ -6,14 +8,48 @@ interface Props {
 
 const Color = ({ color }: Props) => {
   const textColor = getTextColor(color);
+  const [colorName, setColorName] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    const getColorName = (color: string) => {
+      axios
+        .get(`https://www.thecolorapi.com/id?hex=${color}`)
+        .then(res => {
+          const name = res.data.name.value as string;
+          setColorName(name);
+          setIsFetching(false);
+          return;
+        })
+        .catch(err => {
+          console.error(err);
+          setColorName("Color");
+          setIsFetching(false);
+          return;
+        });
+    };
+
+    getColorName(color);
+  }, [color]);
   return (
     <div
       style={{ backgroundColor: `#${color}` }}
       className={`flex justify-center items-end pb-28 w-full h-full`}
     >
-      <h1 style={{ color: textColor }} className="text-3xl">
-        {color}
-      </h1>
+      {!isFetching && (
+        <div className="flex flex-col items-center">
+          <h1 style={{ color: textColor }} className="mb-2 text-3xl">
+            {color}
+          </h1>
+          <p
+            style={{ color: textColor }}
+            className="text-sm italic font-light opacity-60"
+          >
+            {colorName}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
