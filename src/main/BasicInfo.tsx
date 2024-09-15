f23b08c6f94aa6efeab30237daece2c051b5d109
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-15 10:24:09 -0400

    Add Basic Info Section

---
+++ src/main/BasicInfo.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  className?: string;
+}
+
+const BasicInfo = ({ className }: Props) => {
+  return (
+    <div className={className}>
+      <div className="relative flex flex-col w-1/2">
+        <h1 className="z-10 text-7xl">
+          <span className="bg-clip-text text-transparent bg-gradient-to-l from-[hsl(from_hsl(var(--accent))_h_s_calc(l_*_0.6))] from-10% to-accent to-80%">
+            Effortless
+          </span>{" "}
+          <div className="relative z-10 inline">
+            Color
+            <div className="absolute -top-16 -left-10 size-[250%] bg-[radial-gradient(closest-corner_at_40%_50%,_hsla(from_hsl(var(--secondary))_h_s_calc(l_*_0.75)_/_0.3)_0%,_rgba(36,_36,_36,_0)_50%)] rotate-12 -z-10" />
+          </div>{" "}
+          Creation.
+        </h1>
+        <p
+          style={{ width: "clamp(45ch, 50%, 75ch)" }}
+          className="mt-4 text-lg text-[hsl(from_hsl(var(--foreground))_h_s_calc(l_*_0.75))]"
+        >
+          Use the space bar to randomize the color palette. If you find a color you
+          <em className="text-primary"> love</em>, simply click to lock it in place
+          and keep it from randomizing.
+        </p>
+      </div>
+      <div>
+        <img
+          src="https://png.pngtree.com/png-vector/20240309/ourmid/pngtree-vivid-colorful-powder-splash-png-image_11899648.png"
+          alt="color smoke"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BasicInfo;
---
+++ src/main/Hero.tsx
@@ -0,0 +1,33 @@
+interface Props {
+  className?: string;
+}
+
+const Hero = ({ className }: Props) => {
+  return (
+    <div className={className}>
+      <div className="relative flex justify-center w-full h-32">
+        <h1
+          style={{ textShadow: "3px 3px 10px rgba(0, 0, 0, 0.5)" }}
+          className="absolute z-10 text-transparent text-9xl"
+        >
+          Inspiration
+        </h1>
+        <h1 className="absolute text-9xl bg-gradient-to-l from-primary from-10% to-accent to-80% bg-clip-text text-transparent z-10">
+          Inspiration
+        </h1>
+      </div>
+      <h2 className="z-10 mt-4 text-4xl">at the press of a button</h2>
+      <button
+        type="button"
+        id="space"
+        className=" text-lg mt-16 bg-gradient-to-b from-primary to-[hsla(from_hsl(var(--primary))_h_s_calc(l_*_0.75)_/_0.5)] px-[3.25rem] py-[0.675rem] rounded-2xl z-10"
+      >
+        Press Space
+      </button>
+      <div className="absolute top-10 left-60 size-[60%] bg-[radial-gradient(closest-corner_at_40%_50%,_hsla(from_hsl(var(--accent))_h_s_calc(l_*_0.75)_/_0.3)_0%,_rgba(36,_36,_36,_0)_50%)] -rotate-[25deg] z-0" />
+      <div className="absolute top-40 -right-10 size-1/2 bg-[radial-gradient(closest-corner_at_40%_50%,_hsla(from_hsl(var(--primary))_h_s_calc(l_*_0.75)_/_0.3)_0%,_rgba(36,_36,_36,_0)_60%)] rotate-6 z-0" />
+    </div>
+  );
+};
+
+export default Hero;
