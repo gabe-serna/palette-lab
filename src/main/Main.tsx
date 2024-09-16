255486982552f7bc565c3edfc7ab75e27e663ff5
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-16 16:57:39 -0400

    Add Footer

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -15,6 +15,17 @@ const Main = () => {
         <AdditionalInfo className="flex flex-col items-center justify-center gap-12 px-24 size-full bg-[hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.08))] bg-[radial-gradient(ellipse,_rgba(0,_0,_0,_0)_50%,_hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.04))_100%)]" />
         <SideNoteDivider className="w-full px-24 h-2/3 bg-[hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.08))] bg-[radial-gradient(ellipse_65%_80%,_rgba(0,_0,_0,_0)_35%,_hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.04))_100%)] z-10" />
         <GalleryInfo className="flex items-center justify-around px-24 size-full bg-[hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.08))] bg-[radial-gradient(ellipse,_rgba(0,_0,_0,_0)_50%,_hsl(from_hsl(var(--primary))_h_calc(s_*_2.5)_calc(l_*_.04))_100%)]" />
+        <footer className="flex mr-auto flex-col items-center justify-center w-[calc(100vw-15rem-10px)] h-32 gap-2 py-6 text-primary-foreground/65">
+          <div className="flex items-center justify-center gap-8">
+            <a href="#">Created by Gabe Serna</a>
+            <a href="#">Github</a>
+          </div>
+          <div>
+            <p className="text-xs italic text-primary-foreground/55">
+              Copyright © 2024 All Rights Reserved. CC BY-NC-ND License.
+            </p>
+          </div>
+        </footer>
       </div>
     </>
   );
