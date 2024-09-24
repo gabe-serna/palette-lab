48cf64d57db1c163db8146f356e7971431dad5dc
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-24 08:51:14 -0400

    Add Responsiveness to Theme Cards

--- src/main/ThemeCard.tsx
+++ src/main/ThemeCard.tsx
@@ -14,15 +14,18 @@ const ThemeCard = ({ theme, name }: Props) => {
         boxShadow: "0px 0px 10px hsl(var(--primary))",
         backgroundColor: `#${colors[0]}`
       }}
-      className="grid items-center pb-1 w-52 h-16 grid-rows-[8fr_2fr] rounded-3xl hover:scale-105 transition-transform hover:cursor-pointer"
+      className={
+        "3xl:w-[16.25rem] 3xl:h-[5rem] 2xl:w-52 2xl:h-16 xl:w-44 xl:h-[3.375rem] w-36 h-11 " +
+        "grid items-center pb-1 grid-rows-[8fr_2fr] rounded-3xl hover:scale-105 transition-transform hover:cursor-pointer"
+      }
     >
-      <div className="flex justify-around w-full h-full row-start-1 px-10 pt-2 mx-auto overflow-hidden rounded-t-3xl">
+      <div className="flex justify-around w-full h-full row-start-1 px-10 pt-[0.375rem] mx-auto overflow-hidden xl:pt-2 rounded-t-3xl">
         {colors.map((color, index) => {
           if (index < 2) return;
           return (
             <div
               key={color}
-              className="rounded-full size-7"
+              className="rounded-full 3xl:size-9 2xl:size-7 xl:size-[1.375rem] size-[1.125rem]"
               style={{ backgroundColor: `#${color}` }}
             />
           );
@@ -30,7 +33,7 @@ const ThemeCard = ({ theme, name }: Props) => {
       </div>
       <figcaption
         style={{ color: `#${colors[1]}` }}
-        className="row-start-2 text-sm text-center"
+        className="row-start-2 text-[0.625rem] xl:text-xs text-center 3xl:text-lg 2xl:text-sm "
       >
         {name}
       </figcaption>
